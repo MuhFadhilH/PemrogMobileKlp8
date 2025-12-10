3399b360c0f6bdae722a32a487075731655dda37
amrullh <168280074+amrullh@users.noreply.github.com>
2025-12-10 21:50:32 +0800

    fix gradle isu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
     namespace = "com.example.biblio"
     compileSdk = flutter.compileSdkVersion
+    
+    // 1. UBAH BAGIAN INI (Mengatur versi NDK secara manual)
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // 2. TAMBAHKAN BARIS INI (Mengaktifkan Desugaring)
+        isCoreLibraryDesugaringEnabled = true
+        
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -21,34 +24,28 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.biblio"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        applicationId = "com.example.contoh" // Pastikan ID ini sesuai keinginanmu
+        minSdk = 33
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // Opsional: Jika error multidex muncul nanti, tambahkan ini:
+        // multiDexEnabled = true 
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
-dependencies {
-    // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
-    // Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
-    // implementation("com.google.firebase:firebase-analytics")
-}
-
 flutter {
     source = "../.."
+}
+
+// 3. TAMBAHKAN BLOK DEPENDENCIES INI DI BAGIAN PALING BAWAH
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
